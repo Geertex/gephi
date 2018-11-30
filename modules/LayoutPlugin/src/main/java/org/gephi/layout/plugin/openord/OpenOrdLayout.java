271a9781846789b850f283290c910130a2853d8e
Unknown <geertex@gmail.com>
2018-11-30 15:54:27 +0100

    Normalized degree quadratic scaling without scaledown

--- modules/LayoutPlugin/src/main/java/org/gephi/layout/plugin/openord/OpenOrdLayout.java
+++ modules/LayoutPlugin/src/main/java/org/gephi/layout/plugin/openord/OpenOrdLayout.java
@@ -155,6 +155,20 @@ public class OpenOrdLayout implements Layout, LongTask {
                 }
             }
 
+            //Normalization degree loading
+            //Normalizes degree to be between 1-2
+            int maxDegree = 0;
+            int minDegree = Integer.MAX_VALUE;
+            for (int i = 0; i < numNodes; i++) {
+                int degree = neighbors[i].size();
+                nodes[i].degree = degree;
+                maxDegree = Math.max(maxDegree, degree);
+                minDegree = Math.min(minDegree, degree);
+            }
+            for (int i = 0; i < numNodes; i++) {
+                nodes[i].degree = 1f+((nodes[i].degree-minDegree)/(maxDegree-minDegree));
+            }
+
             //Reset position
             boolean someFixed = false;
             for (Node n : nodes) {
